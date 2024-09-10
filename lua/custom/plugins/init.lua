c9fd2e6570e599b796a08daa44be7c8630fa1345
BanceDev <lanceaborden@protonmail.com>
2024-09-09 23:07:33 -0400

    updated to scream theme

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -23,4 +23,49 @@ return {
       vim.api.nvim_set_keymap('n', '<leader>c', ':BufferClose<CR>', { noremap = true, silent = true })
     end,
   },
+  {
+    'nvimdev/dashboard-nvim',
+    event = 'VimEnter',
+    config = function()
+      require('dashboard').setup {
+        theme = 'doom', --  theme is doom and hyper default is hyper
+        config = {
+          header = {
+            [[.llll,     .llll ;llllllll,    .;codxdoc,     ;xxxx.     dxxxd .llll.   dxxxo    'xxxx.  ]],
+            [[,MMMMMKc   lMMMM OMMMMXXXXd  cKMMMMMMMMMMWO'   kMMMN'   OMMMN' lMMMM,  ,MMMMMk  ,WMMMM0  ]],
+            [[,MMMMMMMXc lMMMM OMMMM....  KMMMWo'...;OMMMW'   kMMMW' OMMMX.  lMMMM,  0MMMMMMO;WMMMMMM' ]],
+            [[,MMMMxKMMMNKMMMM OMMMMMMMM,.MMMM0      .MMMMx    oMMMN0MMMN.   lMMMM, 'MMMMNMMMMMMWWMMMO ]],
+            [[,MMMM, ,0MMMMMMM OMMMM;;;;. OMMMMk:'',lKMMMN.     oMMMMMMK.    lMMMM, 0MMMW.OMMMMM;oMMMM.]],
+            [[,MMMM,   ,OMMMMM OMMMMMMMMk  ;OWMMMMMMMMMKd.       oMMMMK      lMMMM,.MMMMo  OMMW; .WMMMO]],
+            [[.;;;;.     ';;;; ';;;;;;;;.     .;cllc:,.           ;WMK       .;;;;..;;;;    kW'   ';;;;]],
+            [[                                                     :O                        .         ]],
+            [[                                                                                         ]],
+            [[                                      ⢀⣤⣶⣿⣿⣿⣿⣿⣷⣶⣤⡀                                       ]],
+            [[                                     ⣴⣿⣿⣿⣿⣿⣿⣿⠿⣿⣿⣾⣴⠁⣀                                     ]],
+            [[                                    ⠀⣿⣿⣿⡿⣿⣿⣯⣿⣿⣦⢙⣿⣿⣷⣧⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                 ]],
+            [[                                ⠀ ⠀⠀⠀⢸⣿⣿⠃⠹⣿⣿⠀⠙⠻⣧⣿⣿⣷⣿⣧⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀]],
+            [[                     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⡿⠀⠀⠸⣿⣧⡀⠀⠈⠙⠻⢿⣿⡛⠷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀       ]],
+            [[⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                   ⠀⣿⠁⠀⠀⢀⣿⣿⣷⣄⡀⠀⠀⠀⠈⣻⣯⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀       ]],
+            [[                                     ⠀⢿⣄⠀⣀⣾⣿⣿⣿⣿⣿⡶⣶⣶⣾⣿⠟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀       ]],
+            [[                                     ⠀⠀⠙⠿⠿⢿⡇⢀⣈⣉⣻⣿⣶⣶⣶⣦⡦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀       ]],
+            [[                                     ⠀⠀⠀ ⠀⠀⢩⣯⣿⣿⣿⣯⡷⡧⣏⣿⢂⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀       ]],
+            [[                   ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⡏⠀⠀⠀⠈⠻⣿⣟⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀       ]],
+            [[                   ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⡄⠀⠀⠀⠀⠹⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀       ]],
+            [[                                     ⠀⠀⠀⠀⠀⠀⠀⠀⠙⣆⠀⠀⠀⠀⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀       ]],
+            [[                                     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⡄⠀⠀⠀⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀       ]],
+            [[                                     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢷⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀       ]],
+            [[                                     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⡆⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀       ]],
+            [[                                     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀       ]],
+            [[                                     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠲⠚⠀⠀⠀⠀⠀⠀ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀       ]],
+          },
+          center = {
+            { icon = '  ', desc = '   Find File', action = 'Telescope find_files', shortcut = 'SPC f f' },
+            { icon = '  ', desc = '   Bookmarks', action = 'Telescope marks', shortcut = 'SPC f m' },
+          },
+          footer = { 'Do you like scary movies?' },
+        }, --  config used for theme
+      }
+    end,
+    dependencies = { { 'nvim-tree/nvim-web-devicons' } },
+  },
 }
